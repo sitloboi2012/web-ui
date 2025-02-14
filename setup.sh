617ac50c3ced29af32a814ad98401679acdb8a9e
sitloboi2012 <huyvo6812@gmail.com>
2025-02-14 21:39:08 +0700

    update transformers git hash version

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ pip install -r requirements.txt
 playwright install
 playwright install-deps
 sudo apt upgrade -y
-pip install git+https://github.com/huggingface/transformers.git
+pip install --upgrade git+https://github.com/huggingface/transformers.git@336dc69d63d56f232a183a3e7f52790429b871ef
 pip install vllm --pre --extra-index-url https://wheels.vllm.ai/nightly
+pip install qwen-vl-utils[decord]==0.0.8
 mkdir models
